634c96b754124c54861a1554e53ff2f379817532
andronikoss <and.stepanyan@gmail.com>
2014-07-23 18:28:13 +0200

    Update server.R

--- server.R
+++ server.R
@@ -8,26 +8,26 @@ shinyServer(function(input, output){
     Number.of.Intervalls <- input$Number.of.Intervalls
     act <- input$action
                         
-    Ausreisser <- sim.int(Number.of.Observation= Number.of.Observation, alpha=0.2,
-                    beta=0.13, Significance.Level = Significance.Level, 
-                    Standard.Deviation =Standard.Deviation,
+    Ausreisser <- sim.int(Number.of.Observation = Number.of.Observation, alpha = 0.2,
+                    beta = 0.13, Significance.Level = Significance.Level, 
+                    Standard.Deviation = Standard.Deviation,
                     Number.of.Intervalls = Number.of.Intervalls, act = act)
-    text(-0.5,1.12, paste("Stichproben:",Number.of.Intervalls), cex = 1.2)
-    text(-0.5,1.08, paste("Ausrei\u00DFer:",Ausreisser), col="red", cex = 1.2)
-    text(-0.563,1.04, paste("Anteil der Ausrei\u00DFer:",
+    text(-0.5, 1.12, paste("Stichproben:", Number.of.Intervalls), cex = 1.2)
+    text(-0.5, 1.08, paste("Ausrei\u00DFer:", Ausreisser), col = "red", cex = 1.2)
+    text(-0.563, 1.04, paste("Anteil der Ausrei\u00DFer:",
         round(Ausreisser/Number.of.Intervalls, 2)), cex = 1.2)
                         
     output$downloadPlot <- downloadHandler(
-    filename = function() { paste("intervall", '.jpg', sep='') },
+    filename = function() { paste("intervall", '.jpg', sep = '') },
     content = function(file) {
                 jpeg(file,width = 1000, height = 600)
-                print(sim.int(Number.of.Observation= Number.of.Observation, alpha=0.2,
-                    beta=0.13, Significance.Level = Significance.Level, 
-                    Standard.Deviation =Standard.Deviation,
+                print(sim.int(Number.of.Observation = Number.of.Observation, alpha = 0.2,
+                    beta = 0.13, Significance.Level = Significance.Level, 
+                    Standard.Deviation = Standard.Deviation,
                     Number.of.Intervalls = Number.of.Intervalls, act = act)) 
-                    text(-0.5,1.12, paste("Stichproben:",Number.of.Intervalls), cex = 1.2)
-                    text(-0.5,1.08, paste("Ausrei\u00DFer:",Ausreisser), col="red", cex = 1.2)
-                    text(-0.563,1.04, paste("Anteil der Ausrei\u00DFer:",
+                    text(-0.5, 1.12, paste("Stichproben:", Number.of.Intervalls), cex = 1.2)
+                    text(-0.5, 1.08, paste("Ausrei\u00DFer:", Ausreisser), col = "red", cex = 1.2)
+                    text(-0.563, 1.04, paste("Anteil der Ausrei\u00DFer:",
                     round(Ausreisser/Number.of.Intervalls, 2)), cex = 1.2)
                 dev.off()
             })
